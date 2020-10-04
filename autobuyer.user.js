fbd6573dd739ada6b8441892d4034840f384ec26
fitghosty125 <58052733+fitghosty125@users.noreply.github.com>
2020-10-04 21:57:42 +0200

    Update autobuyer.user.js

--- autobuyer.user.js
+++ autobuyer.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         FUT20 Autobuyer
+// @name         FUT21 Autobuyer
 // @namespace    http://tampermonkey.net/
 // @version      0.6
 // @updateURL    https://github.com/oRastor/fut20-web-app/raw/master/autobuyer.user.js
-// @description  try to take over the world!
-// @author       Rastor
-// @match        https://www.easports.com/uk/fifa/ultimate-team/web-app/*
-// @match        https://www.easports.com/fifa/ultimate-team/web-app/*
+// @description  Big money
+// @author       Bart Koopman
+// @match        https://www.ea.com/en-gb/fifa/ultimate-team/web-app/*
+// @match        https://www.ea.com/fifa/ultimate-team/web-app/*
 // @grant        none
 // ==/UserScript==
 
